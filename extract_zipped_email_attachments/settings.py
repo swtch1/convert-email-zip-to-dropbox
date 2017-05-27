4bb3790da42ae3d986019e709b9c8dce96ac036c
212563094 <joshua.thornton@ge.com>
2017-05-26 23:11:10 -0400

    anyconfig broke on me.. I don't know why. Converting to pyyaml.

--- extract_zipped_email_attachments/settings.py
+++ extract_zipped_email_attachments/settings.py
@@ -1,8 +1,9 @@
-import anyconfig
+import yaml
 
 import os
 
 CUR_DIR = os.path.dirname(__file__)
-
-config = anyconfig.load(os.path.join(CUR_DIR, 'config.yml'))
-auth = anyconfig.load(os.path.join(CUR_DIR, 'auth.yml'))
+with open(os.path.join(CUR_DIR, 'config.yml')) as f:
+    config = yaml.load(f)
+with open(os.path.join(CUR_DIR, 'auth.yml')) as f:
+    auth = yaml.load(f)
