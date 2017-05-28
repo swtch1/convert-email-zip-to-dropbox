4eb1ceb65d39d683538b63d6f76a2e71bae50690
212563094 <joshua.thornton@ge.com>
2017-05-28 13:28:46 -0400

    Using libraries subject search instead of regex.

--- extract_zipped_email_attachments/main.py
+++ extract_zipped_email_attachments/main.py
@@ -17,9 +17,13 @@ if __name__ == '__main__':
         dest_folder = src_folder + config['reports']['folder_suffix']
         mail.ensure_mail_folder_exists(imap_session, dest_folder)
 
-        src_message_ids = mail.get_message_ids(imap_session, src_folder)
-        src_messages_metadata = mail.get_messages_metadata(imap_session, src_message_ids) if src_message_ids != [''] else {}
-        dest_message_ids = mail.get_message_ids(imap_session, dest_folder)
+        src_message_ids = mail.get_message_ids(session=imap_session,
+                                               folder=src_folder,
+                                               search_criteria='(FROM "{}")'.format(auth['email']['recv_reports_from_address']))
+        src_messages_metadata = mail.get_messages_metadata(session=imap_session,
+                                                           message_ids=src_message_ids) if src_message_ids != [''] else {}
+        dest_message_ids = mail.get_message_ids(session=imap_session,
+                                                folder=dest_folder)
         dest_messages_metadata = mail.get_messages_metadata(imap_session, dest_message_ids) if dest_message_ids != [''] else {}
 
         unique_subjects = get_unique_dict_keys(src_messages_metadata, dest_messages_metadata)
@@ -27,8 +31,6 @@ if __name__ == '__main__':
         with tempfile.TemporaryDirectory() as downloads_dir:
             for subject in unique_subjects:
                 attachment = mail.download_attachment(imap_session, src_folder, src_messages_metadata[subject]['message_id'], downloads_dir)
-                if not re.match(config['reports']['subject_match_regex'], subject.lower()):
-                    continue
                 if not zipfile.is_zipfile(attachment):
                     continue
                 zipped_file = ZippedFile(attachment, password=auth['zip']['password'])
@@ -42,5 +44,5 @@ if __name__ == '__main__':
                                         folder=dest_folder,
                                         subject=subject,
                                         to_address=auth['email']['address'],
-                                        from_address=auth['email']['reports_from_address'],
+                                        from_address=auth['email']['send_reports_from_address'],
                                         attachment=fully_qualified_pdf)
