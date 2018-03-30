c3670786fc0a180c805842e0a16500f77218aa75
swtch1 <joshua.thornton@ge.com>
2018-03-30 00:36:35 -0400

    Removing circular imports.  How did this even work before?

--- extract_zipped_email_attachments/settings.py
+++ extract_zipped_email_attachments/settings.py
@@ -2,12 +2,9 @@ import yaml
 
 import os
 
-from extract_zipped_email_attachments.logger import log
 
 CUR_DIR = os.path.dirname(__file__)
-log.debug('parsing config file')
 with open(os.path.join(CUR_DIR, 'config.yml')) as f:
     config = yaml.load(f)
-log.debug('parsing auth file')
 with open(os.path.join(CUR_DIR, 'auth.yml')) as f:
     auth = yaml.load(f)
